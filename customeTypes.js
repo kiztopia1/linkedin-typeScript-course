77a70a5603950933d97061a3b2f3b14f36ddf1bd
kiztopia1 <62418642+kiztopia1@users.noreply.github.com>
2021-10-19 08:13:42 -0700

    adding simple interface

---
+++ customeTypes.js
@@ -0,0 +1,3 @@
+// interfaces
+// lets try making new object and see how ts tell us what to do
+var todo = { name: /* we can only add string here */ 'abebe', completed: false };
---
+++ customeTypes.ts
@@ -0,0 +1,13 @@
+// interfaces
+
+// notes
+//  * interfaces will not be added to the main js file they are used for runtime check  
+interface Todo {
+    name: string;
+    completed?: boolean;
+    // adding ? after the type name it will make it optional  
+}
+
+// lets try making new object and see how ts tell us what to do
+
+var todo: Todo = {name: /* we can only add string here */ 'abebe', completed: false };
