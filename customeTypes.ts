c95975868e96df49882c81c0df55ba3b186d81ff
kiztopia1 <62418642+kiztopia1@users.noreply.github.com>
2021-10-19 08:31:01 -0700

    lesson 2 adding interface to functions

--- customeTypes.ts
+++ customeTypes.ts
@@ -10,4 +10,17 @@ interface Todo {
 
 // lets try making new object and see how ts tell us what to do
 
-var todo: Todo = {name: /* we can only add string here */ 'abebe', completed: false };
+var todo: Todo = {name: /* we can only add string here */ 'abebe', completed: false }; 
+
+// lesson 2
+
+interface jQuery {
+    (selector: string): HTMLElement;
+    version: number;//now version will be always a number
+}
+
+var $ = <jQuery>function(selector) {
+
+}
+$.version = 12.1
+$('.main')
